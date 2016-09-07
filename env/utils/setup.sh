3a3267a30191d2f51ab5b29de57f520bc7a2734c
Emanuel F. Oliveira <emfolg@gmail.com>
2016-09-07 14:05:05 +0000

    setup.sh -- fixed viewers repo URL

--- env/utils/setup.sh
+++ env/utils/setup.sh
@@ -17,7 +17,7 @@ declare tjn_dir tjn_url="https://github.com/tj/n"
 declare meteor_sh meteor_url="https://install.meteor.com/"
 # git/ohif vars
 declare git_user_name git_user_email
-declare ohif_dir="/home/ohif/src" ohif_url="https://github.com/OHIF/Viewers"
+declare ohif_dir="/home/ohif/src" ohif_url="https://github.com/OHIF/Viewers.git"
 # yum var
 declare -a yum_pkgs_missing yum_pkgs_list=( tree vim curl git make gcc g++:gcc-c++ )
 declare yum_pkg_id yum_pkg_cmd
